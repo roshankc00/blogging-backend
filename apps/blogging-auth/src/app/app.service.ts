7610f4e414d6c86810a7e225581a0ad986ae4d64
Roshan karki <rohitkc8848@gmail.com>
2026-05-21 03:21:36 +0545

    feat:resolve the prisma issue according to the docs fix

--- apps/blogging-auth/src/app/app.service.ts
+++ apps/blogging-auth/src/app/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) { }
   getData(): { message: string } {
     return { message: 'Hello API' };
   }
